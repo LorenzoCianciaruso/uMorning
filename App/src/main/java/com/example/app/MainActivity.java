f1c37d4942084d16b60495b627ebbfd93dd96996
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2013-12-17 16:32:01 +0100

    primo commit

--- App/src/main/java/com/example/app/MainActivity.java
+++ App/src/main/java/com/example/app/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+//primo commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
