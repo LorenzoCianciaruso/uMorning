c7c81c2f3fb3b2e975e24159f026719f2fdbf5e4
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-01-11 14:39:56 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	App/src/main/AndroidManifest.xml

--- App/src/main/java/com/example/app/MainActivity.java
+++ App/src/main/java/com/example/app/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.app;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.Provider;
 import java.util.Calendar;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -16,6 +29,16 @@ import android.widget.TimePicker;
 import android.widget.DatePicker;
 import android.widget.Button;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
 import java.lang.Thread;
 
 
@@ -32,6 +55,9 @@ public class MainActivity extends Activity {
 
     private Button btnSetAlarm;
 
+    private double latitude=0;
+    private double longitude=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,20 +72,16 @@ public class MainActivity extends Activity {
         //Intent gpsIntent = new Intent(this, GpsLocalizationService.class);
         //startService(gpsIntent);
         /*
-        double latitude=0;
-        double longitude=0;
         GpsLocalizationService gps = new GpsLocalizationService(this);
 
         if(!gps.canGetLocation()){
             gps.showSettingsAlert();
-
 
             if(gps.canGetLocation()){
             // gps enabled} // return boolean true/false
             latitude = gps.getLatitude(); // returns latitude
             longitude = gps.getLongitude(); // returns longitude
             }
-
 
         }else{
 
@@ -69,15 +91,17 @@ public class MainActivity extends Activity {
 
         }
 
-
         Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
 */
 
+
+
+        /*
         //MeteoService, richiesta informazioni meteo
         Intent meteoIntent = new Intent(this, MeteoService.class);
        // meteoIntent.putExtra("latitude", latitude);
        // meteoIntent.putExtra("longitude", longitude);
-        startService(meteoIntent);
+        startService(meteoIntent);*/
 
 
         //prende ora della sveglia dal date picker
@@ -107,6 +131,39 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+
+
+
+
+    @Override
+    public void onResume(){
+
+      /*  GpsLocalizationService gps = new GpsLocalizationService(MainActivity.this);
+
+        // check if GPS enabled
+        if(gps.canGetLocation()){
+
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+
+
+            // \n is for new line
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+
+            new AsyncTaskMeteoRequest().execute(latitude,longitude);
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }*/
+
+
+
+    }
+
 
 
     @Override
@@ -143,6 +200,73 @@ public class MainActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    private class AsyncTaskMeteoRequest extends AsyncTask<Double, Void, String> {
+
+        @Override
+        protected String doInBackground(Double... params) {
+
+            double latitude = params[0];
+            double longitude = params[1];
+
+            String url = "https://api.metwit.com/v2/weather/?location_lat="+latitude+"&location_lng="+longitude;
+
+            try{
+
+            HttpParams httpParameters = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParameters, 5000);
+            HttpConnectionParams.setSoTimeout(httpParameters, 5000);
+
+            HttpClient client = new DefaultHttpClient(httpParameters);
+            HttpGet request = new HttpGet(url);
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+
+            System.out.println("Risposta: "+convertStreamToString(is));
+
+
+            //Do something with the response
+        }
+        catch (IOException e) {
+
+            Log.e("Tag", "Could not get HTML: " + e.getMessage());
+        }
+            //TODO parsing json per estrarre url icona località temperatura
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String url){
+            //TODO update icona meteo
+
+        }
+
+        private String convertStreamToString(InputStream is) {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
+        }
+
+
+
     }
 
 
