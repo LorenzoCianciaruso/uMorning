45bf677b3ec0412f66142ff58d88264ea08755ca
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-03-20 18:01:51 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	App/src/main/AndroidManifest.xml

--- App/src/main/java/com/example/umorning/AccountManagerActivity.java
+++ App/src/main/java/com/example/umorning/AccountManagerActivity.java
@@ -1,11 +1,27 @@
 package com.example.umorning;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AccountManagerActivity extends ActionBarActivity {
@@ -49,7 +65,45 @@ public class AccountManagerActivity extends ActionBarActivity {
         startActivity(intent);
         finish();
 
+        postData();
 
+
+
+
+    }
+
+    public void postData() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+
+        HttpPost httppost = new HttpPost("https://www.eventbrite.com/oauth/token");
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+// then you use
+        String access = prefs.getString("EventbriteToken", null);
+
+
+        try {
+            // Add your data
+
+
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("code", access));
+            nameValuePairs.add(new BasicNameValuePair("client_secret", "IAHAVFS5X7LLPMSPDYAW63LAZPFWGL6MHBOULWP363TRPVOG5V"));
+            nameValuePairs.add(new BasicNameValuePair("client_id","AWF7I3D2E3CAVX6QNW"));
+            nameValuePairs.add(new BasicNameValuePair("grant_tyoe", "authorization_code"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            //String result = EntityUtils.toString(response);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 
     public void facebookAuth(View view){
