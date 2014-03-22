5ad93d060f8ae8ba9255e8a0ccf04cb79c156dbc
endribregu <endri.bregu@live.com>
2014-03-22 16:24:48 +0100

    icona settings

--- App/src/main/java/com/example/umorning/MainActivity.java
+++ App/src/main/java/com/example/umorning/MainActivity.java
@@ -64,7 +64,6 @@ public class MainActivity extends FragmentActivity implements
         });
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
