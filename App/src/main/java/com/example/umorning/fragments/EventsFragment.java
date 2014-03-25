46449fc8f3ce270efada1303402c1735875470ab
Francesco <fdifo@hotmail.it>
2014-03-25 11:15:58 +0100

    spostata la roba nei fragment giusti

--- App/src/main/java/com/example/umorning/fragments/EventsFragment.java
+++ App/src/main/java/com/example/umorning/fragments/EventsFragment.java
@@ -1,13 +1,8 @@
 package com.example.umorning.fragments;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,27 +10,17 @@ import android.view.ViewGroup;
 
 import com.example.umorning.R;
 import com.example.umorning.external_services.Eventbrite;
+import com.example.umorning.internal_services.EventService;
 import com.example.umorning.model.Event;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EventsFragment extends Fragment {
 
-    /*
-    // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[]{
-            CalendarContract.Calendars._ID,                           // 0
-            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
-    };
-    */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         setHasOptionsMenu(true);
 
         super.onCreate(savedInstanceState);
@@ -50,8 +35,8 @@ public class EventsFragment extends Fragment {
             //TODO notlogged
         }else{
         new AsyncTaskEventbrite().execute(token);
+        new AsyncTaskEvent().execute();
         }
-
         return rootView;
     }
 
@@ -60,14 +45,24 @@ public class EventsFragment extends Fragment {
 
         @Override
         protected List<Event> doInBackground(String... params) {
-
             String token = params[0];
-
             Eventbrite eve = new Eventbrite(token);
             eve.getEventbriteOrders();
             List<Event> eventList = eve.getEventList();
             return eventList;
+        }
+        @Override
+        protected void onPostExecute(List<Event> params){
+        }
+    }
 
+    private class AsyncTaskEvent extends AsyncTask<Void, Void, List<Event>>{
+
+        @Override
+        protected List<Event> doInBackground(Void...params) {
+            EventService eve = new EventService();
+            List<Event> eventList = eve.getEvent();
+            return eventList;
         }
 
         @Override
@@ -75,69 +70,4 @@ public class EventsFragment extends Fragment {
 
         }
     }
-
-    private class AsyncTaskEventRetrive extends AsyncTask<Void, Void, List<Event> > {
-
-        @Override
-        protected List<Event> doInBackground(Void...params) {
-
-            /*
-            // Run query
-            Cursor cur = null;
-            ContentResolver cr = getContentResolver();
-            Uri uri = CalendarContract.Calendars.CONTENT_URI;
-            String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-                    + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
-                    + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-            String[] selectionArgs = new String[]{"lory90@gmail.com", "com.google",};
-
-            // Submit the query and get a Cursor object back.
-            String selection = "(1=?)";
-            String[] selectionArgs = new String[]{"1"};
-            cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-
-
-            String[] projection =
-                    new String[]{
-                            CalendarContract.Calendars._ID,
-                            CalendarContract.Calendars.NAME,
-                            CalendarContract.Calendars.ACCOUNT_NAME,
-                            CalendarContract.Calendars.ACCOUNT_TYPE};
-            Cursor calCursor =
-                    getContentResolver().
-                            query(CalendarContract.Calendars.CONTENT_URI,
-                                    projection,
-                                    CalendarContract.Calendars.VISIBLE + " = 1",
-                                    null,
-                                    CalendarContract.Calendars._ID + " ASC");
-            if (calCursor.moveToFirst()) {
-                do {
-                    long id = calCursor.getLong(0);
-                    String displayName = calCursor.getString(1);
-                    System.out.println("Ecco i campi" + calCursor.getString(0) + calCursor.getString(1) + calCursor.getString(2) + calCursor.getString(3));
-                } while (calCursor.moveToNext());
-            }
-            */
-            Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
-/*
-            Cursor mCursor;
-            String[] projection = new String[]
-                    {CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND};
-
-            mCursor = getContentResolver().query(
-                    builder.build(), projection, CalendarContract.Instances.CALENDAR_ID + " = ?",
-                    new String[]{"1"}, null);
-            if (mCursor.moveToFirst()) {
-                do {
-                    System.out.println("Ecco i papa" + mCursor.getString(0));
-                } while (mCursor.moveToNext());
-            }
-            */
-            List<Event> events = new ArrayList<Event>();
-
-            return events;
-        }
-
-    }
-
 }
---
+++ App/src/main/java/com/example/umorning/internal_services/EventService.java
@@ -0,0 +1,72 @@
+package com.example.umorning.internal_services;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import com.example.umorning.model.Event;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventService {
+    public List<Event> getEvent(){
+
+            /*
+            // Run query
+            Cursor cur = null;
+            ContentResolver cr = getContentResolver();
+            Uri uri = CalendarContract.Calendars.CONTENT_URI;
+            String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                    + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                    + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+            String[] selectionArgs = new String[]{"lory90@gmail.com", "com.google",};
+
+            // Submit the query and get a Cursor object back.
+            String selection = "(1=?)";
+            String[] selectionArgs = new String[]{"1"};
+            cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+
+
+            String[] projection =
+                    new String[]{
+                            CalendarContract.Calendars._ID,
+                            CalendarContract.Calendars.NAME,
+                            CalendarContract.Calendars.ACCOUNT_NAME,
+                            CalendarContract.Calendars.ACCOUNT_TYPE};
+            Cursor calCursor =
+                    getContentResolver().
+                            query(CalendarContract.Calendars.CONTENT_URI,
+                                    projection,
+                                    CalendarContract.Calendars.VISIBLE + " = 1",
+                                    null,
+                                    CalendarContract.Calendars._ID + " ASC");
+            if (calCursor.moveToFirst()) {
+                do {
+                    long id = calCursor.getLong(0);
+                    String displayName = calCursor.getString(1);
+                    System.out.println("Ecco i campi" + calCursor.getString(0) + calCursor.getString(1) + calCursor.getString(2) + calCursor.getString(3));
+                } while (calCursor.moveToNext());
+            }
+            */
+        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+
+        Cursor mCursor;
+        String[] projection = new String[]
+                {CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND};
+
+        mCursor = getContentResolver().query(
+                builder.build(), projection, CalendarContract.Instances.CALENDAR_ID + " = ?",
+                new String[]{"1"}, null);
+        List<Event> events = new ArrayList<Event>();
+        if (mCursor.moveToFirst()) {
+            do {
+                Event event = new Event(CalendarContract.Events.TITLE, CalendarContract.Events.ORIGINAL_ID, CalendarContract.Events.ORGANIZER, CalendarContract.Events.EVENT_LOCATION);
+                events.add(event);
+            } while (mCursor.moveToNext());
+        }
+        for (int i=0;i<events.size();i++){
+            Event e = events.get(i);
+            System.out.println (e.getName()+e.getLocationName()+e.getOrganizerName()+e.getId());
+        }
+        return events;
+    }
+}
