ab82f5f199180408f2ad24e0c886aebfeb575a2a
Francesco <fdifo@hotmail.it>
2014-03-28 10:43:01 +0100

    serializzato

--- App/src/main/java/com/example/umorning/model/Alarm.java
+++ App/src/main/java/com/example/umorning/model/Alarm.java
@@ -1,10 +1,13 @@
 package com.example.umorning.model;
 
+import java.io.Serializable;
 
-public class Alarm {
+public class Alarm implements Serializable {
     private int id;
     private Event event;
     private int delay; //tempo che l'utente ci mette a uscire di casa
+    private boolean activated;
+
 
     public int getId() {
         return id;
