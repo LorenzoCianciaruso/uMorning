9b206b1c344ac3dcc349efc58c577bd51b72cfc7
Francesco <fdifo@hotmail.it>
2014-03-28 16:00:38 +0100

    nuovo allarma

--- App/src/main/java/com/example/umorning/model/Alarm.java
+++ App/src/main/java/com/example/umorning/model/Alarm.java
@@ -1,28 +1,82 @@
 package com.example.umorning.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Alarm implements Serializable {
+
     private int id;
-    private Event event;
     private int delay; //tempo che l'utente ci mette a uscire di casa
-    private boolean activated;
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private String startLatitude;
+    private String startLongitude;
+    private String endLatitude;
+    private String endLongitude;
+    private String locationName;
+
+    //TODO il costruttore dovrà prendere un evento e settare i campi
+    //TODO settare l'id in maniera intelligente (hash dei campi??)
 
 
     public int getId() {
         return id;
     }
 
-    public Event getEvent() {
-        return event;
-    }
-
     public int getDelay() {
         return delay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getStartLatitude() {
+        return startLatitude;
+    }
+
+    public String getStartLongitude() {
+        return startLongitude;
+    }
+
+    public String getEndLatitude() {
+        return endLatitude;
+    }
+
+    public String getEndLongitude() {
+        return endLongitude;
+    }
+
+    public String getLocationName() {
+        return locationName;
     }
 
     public boolean isActivated() {
         return activated;
     }
+
+    public Date getDate() {
+        return date;
+    }
+
+    private Date date;
+    private boolean activated;
+
+
+
 
 }
