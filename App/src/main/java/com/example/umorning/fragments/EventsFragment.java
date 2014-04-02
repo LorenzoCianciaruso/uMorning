ff3414df2da057fe3fb6adec3224a9232a355a5c
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-04-02 10:48:29 +0200

    godo

--- App/src/main/java/com/example/umorning/fragments/EventsFragment.java
+++ App/src/main/java/com/example/umorning/fragments/EventsFragment.java
@@ -19,6 +19,8 @@ import java.util.List;
 public class EventsFragment extends Fragment {
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
