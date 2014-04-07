c63fee4317b1c8adce8894d7610e9c6612c4f0ce
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-04-07 15:08:15 +0200

    godo

--- App/src/main/java/com/example/umorning/activities/MainActivity.java
+++ App/src/main/java/com/example/umorning/activities/MainActivity.java
@@ -103,4 +103,8 @@ public class MainActivity extends FragmentActivity implements
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+    public void prova(MenuItem item){
+        Intent intent = new Intent(this, EventDetailsActivity.class);
+        startActivity(intent);
+    }
 }
