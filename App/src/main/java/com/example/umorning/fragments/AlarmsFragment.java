72a5878415fad06ea3a4f1da8faef4bb42aa32b2
endribregu <endri.bregu@live.com>
2014-04-08 11:39:21 +0200

    grafica

--- App/src/main/java/com/example/umorning/fragments/AlarmsFragment.java
+++ App/src/main/java/com/example/umorning/fragments/AlarmsFragment.java
@@ -1,5 +1,6 @@
 package com.example.umorning.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.umorning.R;
+import com.example.umorning.activities.AlarmAddNewActivity;
 import com.example.umorning.model.Alarm;
 import com.example.umorning.model.DatabaseHelper;
 
@@ -45,6 +47,12 @@ public class AlarmsFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view,
                                     int i, long l) {
+
+                Intent myIntent = new Intent(getActivity(), AlarmAddNewActivity.class);
+                //alarms.get(i);
+                myIntent.putExtra("alarmId", alarms.get(i).getId());
+                //startActivity(myIntent);
+                startActivityForResult(myIntent,0);
 
             }
         });
