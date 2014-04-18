0000246df8c3000d95b020e0f1c31500a0ef4ff9
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-04-18 11:54:22 +0200

    zs

--- App/src/main/java/com/example/umorning/activities/EventDetailsActivity.java
+++ App/src/main/java/com/example/umorning/activities/EventDetailsActivity.java
@@ -1,8 +1,6 @@
 package com.example.umorning.activities;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.ShareCompat;
@@ -13,29 +11,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
-
 import com.example.umorning.R;
 import com.example.umorning.external_services.Facebook;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 
 public class EventDetailsActivity extends FragmentActivity {
 
     private GoogleMap mMap;
-    private TextView nameView;
-    private TextView dateTimeView;
-    private TextView placeView;
-    private TextView urlView;
-    private TextView organizerView;
-    private MenuItem shareFb;
     private String url;
     private String name;
     private String time;
@@ -58,13 +44,11 @@ public class EventDetailsActivity extends FragmentActivity {
         time = getIntent().getStringExtra("date");
         String organizer = getIntent().getStringExtra("organizer");
 
-        nameView = (TextView) findViewById(R.id.event_name);
-        dateTimeView = (TextView) findViewById(R.id.date_time);
-        placeView = (TextView) findViewById(R.id.place);
-        urlView = (TextView) findViewById(R.id.url);
-        organizerView = (TextView) findViewById(R.id.organizer);
-
-        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        TextView nameView = (TextView) findViewById(R.id.event_name);
+        TextView dateTimeView = (TextView) findViewById(R.id.date_time);
+        TextView placeView = (TextView) findViewById(R.id.place);
+        TextView urlView = (TextView) findViewById(R.id.url);
+        TextView organizerView = (TextView) findViewById(R.id.organizer);
 
         nameView.setText(name);
         dateTimeView.setText(time);
@@ -78,7 +62,6 @@ public class EventDetailsActivity extends FragmentActivity {
 
         String text = "<a href=" + url + " \">Link to the event</a>";
         urlView.setMovementMethod(LinkMovementMethod.getInstance());
-
         urlView.setText(Html.fromHtml(text));
         organizerView.setText(organizer);
 
@@ -91,7 +74,7 @@ public class EventDetailsActivity extends FragmentActivity {
         super.onResume();
         //se esiste sessione attiva
         fb = new Facebook(this);
-        setUpMapIfNeeded();
+        //setUpMapIfNeeded();
     }
 
     @Override
@@ -102,7 +85,7 @@ public class EventDetailsActivity extends FragmentActivity {
         Intent shareIntent = ShareCompat.IntentBuilder.from(this)
                 .setType("text/plain").setText("").getIntent();
         mShareActionProvider.setShareIntent(shareIntent);
-        shareFb = menu.findItem(R.id.fb_share);
+        MenuItem shareFb = menu.findItem(R.id.fb_share);
         if (!fb.isLogged()) {
             shareFb.setVisible(false);
             shareFb.setEnabled(false);
@@ -117,20 +100,22 @@ public class EventDetailsActivity extends FragmentActivity {
             fb.publishStory(this, name, place, url, time);
         } else if (id == R.id.add) {
 
+        //TODO QUI
 
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void setUpMapIfNeeded() {
-
-        if (mMap != null) {
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude), 15));
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(latitude, longitude))
-                    .title(name));
+        if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (mMap != null) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15));
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(latitude, longitude))
+                        .title(name));
+            }
         }
     }
-
 
 }
