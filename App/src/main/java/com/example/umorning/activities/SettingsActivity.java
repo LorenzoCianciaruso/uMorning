415a32776db806aabcbf52bb176f1b216b4a27a8
endribregu <endri.bregu@live.com>
2014-04-23 16:55:09 +0200

    icone grige, suonerie

--- App/src/main/java/com/example/umorning/activities/SettingsActivity.java
+++ App/src/main/java/com/example/umorning/activities/SettingsActivity.java
@@ -1,13 +1,8 @@
 package com.example.umorning.activities;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -18,15 +13,18 @@ import android.widget.TextView;
 
 import com.example.umorning.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsActivity extends Activity {
 
     private NumberPicker delayPicker;
     private NumberPicker refreshPicker;
+    //ringtone
+    private Uri ringtoneUri;
+    private Ringtone ringtone;
 
     private TextView selectedRingtone;
+
     private List<String> ringtones;
     private List<Uri> ringtonesListUri;
     String ringtonePicked;
@@ -51,69 +49,11 @@ public class SettingsActivity extends Activity {
         saved = prefs.getLong("REFRESH", 60);
         refreshPicker.setValue((int) saved);
 
-        /*
-        //ringtone
-        ringtonesListUri = new ArrayList<Uri>();
-        ringtones = new ArrayList<String>();
         selectedRingtone = (TextView) findViewById(R.id.selectedRingtone);
-        RingtoneManager ringtoneMgr = new RingtoneManager(this);
-        ringtoneMgr.setType(RingtoneManager.TYPE_ALARM);
-
-        Uri uriDefault = Uri.parse( prefs.getString("TONE", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString()));
-        Ringtone defaultRingtone = RingtoneManager.getRingtone(this, uriDefault);
-        String ringtoneName = defaultRingtone.getTitle(this);
-        selectedRingtone.setText(ringtoneName);
         selectedRingtone.setClickable(true);
-
-        Cursor alarmsCursor = ringtoneMgr.getCursor();
-        int alarmsCount = alarmsCursor.getCount();
-        if (alarmsCursor.moveToFirst()) {
-            Uri[] alarms = new Uri[alarmsCount];
-            while (!alarmsCursor.isAfterLast() && alarmsCursor.moveToNext()) {
-                int i = alarmsCursor.getPosition();
-                alarms[i] = ringtoneMgr.getRingtoneUri(i);
-                ringtonesListUri.add(alarms[i]);
-                Ringtone ringtone = RingtoneManager.getRingtone(this, alarms[i]);
-                ringtones.add(ringtone.getTitle(this));
-            }
-            alarmsCursor.close();
-
-        }
-
-    }
-
-    public class SelectRingtone extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            CharSequence[] cs = ringtones.toArray(new CharSequence[ringtones.size()]);
-            builder.setTitle("Select Ringtone").setItems(cs, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    ringtonePicked = ringtones.get(which);
-                    selectedRingtone.setText(ringtonePicked);
-                }
-            });
-            return builder.create();
-        }
-    }*/
-
-        String uri = null;
-        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,
-                RingtoneManager.TYPE_NOTIFICATION);
-        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
-
-        SharedPreferences settings = getSharedPreferences("uMorning", 0);
-        uri = settings.getString("ringtone", "none");
-
-        if (!uri.equals("none")) {
-            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI,
-                    Uri.parse(uri));
-        } else {
-            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI,
-                    (Uri) null);
-        }
-        startActivityForResult(intent, 999);
+        ringtoneUri = Uri.parse(prefs.getString("TONE", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString()));
+        ringtone = RingtoneManager.getRingtone(this, ringtoneUri);
+        selectedRingtone.setText(ringtone.getTitle(this));
 
     }
 
@@ -126,21 +66,53 @@ public class SettingsActivity extends Activity {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putLong("DELAY", userDelay);
         editor.putLong("REFRESH", refreshRate);
-        Uri uri=fromTitleToUri(ringtonePicked);
-        editor.putString("TONE", uri.toString());
+        editor.putString("TONE", ringtoneUri.toString());
         editor.commit();
         finish();
     }
 
-    /*public void selectRingtone(View view) {
-        SelectRingtone r = new SelectRingtone();
-        r.show(getFragmentManager(), "ringtone");
-    }*/
+    public void selectRingtone(View view) {
+        String uri = null;
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        //intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,
+        // RingtoneManager.TYPE_ALARM);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
 
-    private Uri fromTitleToUri(String title) {
-        for (Uri u : ringtonesListUri)
-            if (RingtoneManager.getRingtone(this, u).getTitle(this).equals(title))
-                return u;
-        return null;
+        SharedPreferences settings = getSharedPreferences("uMorning", 0);
+        uri = settings.getString("ringtone", "none");
+
+        if (!uri.equals("none")) {
+            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI,
+                    Uri.parse(uri));
+            System.out.println();
+        } else {
+            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI,
+                    (Uri) null);
+        }
+        startActivityForResult(intent, 999);
+
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+
+        try {
+            ringtoneUri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+        } catch (NullPointerException e) {
+            SharedPreferences prefs = getSharedPreferences("uMorning", 0);
+            ringtoneUri = Uri.parse(prefs.getString("TONE", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString()));
+        }
+
+        ringtone = RingtoneManager.getRingtone(this, ringtoneUri);
+        selectedRingtone.setText(ringtone.getTitle(this));
+
+        // salva nelle preferenze
+        SharedPreferences prefs = getSharedPreferences("uMorning", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("TONE", ringtoneUri.toString());
+        editor.commit();
+
+    }
+
 }
