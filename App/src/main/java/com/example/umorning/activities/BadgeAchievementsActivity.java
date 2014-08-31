8cae8eb7e9a28c2dec7490c9b1f336a1bfe5d198
LorenzoCianciaruso <lorenzo.cianciaruso@hotmail.it>
2014-08-31 18:33:32 +0200

    UI fixies

--- App/src/main/java/com/example/umorning/activities/BadgeAchievementsActivity.java
+++ App/src/main/java/com/example/umorning/activities/BadgeAchievementsActivity.java
@@ -21,7 +21,7 @@ public class BadgeAchievementsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_badge_achievemts);
+        setContentView(R.layout.activity_badge_achievements);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setDisplayShowHomeEnabled(false);
@@ -45,26 +45,6 @@ public class BadgeAchievementsActivity extends Activity {
             textViews[i].setText(badges.get(i).getName());
         }
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.badge_achievemts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void aquireTextView(){
